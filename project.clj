8f48caf5acc370ddfb67bea6f990d9f913db1c86
James Reeves <jreeves@weavejester.com>
2012-01-03 19:17:41 +0000

    Updated default Clojure version to 1.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject ring-anti-forgery "0.1.0"
   :description "Ring middleware to prevent CSRF attacks"
-  :dependencies [[org.clojure/clojure "1.2.0"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]]
   :dev-dependencies [[ring-mock "0.1.1"]])
