58e75c6ce5db2f50a44d9dc7195a8ed55b27b2a1
James Reeves <jreeves@weavejester.com>
2012-05-01 23:12:26 +0100

    Updated project file to Lein2

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject ring-anti-forgery "0.1.1"
   :description "Ring middleware to prevent CSRF attacks"
   :dependencies [[org.clojure/clojure "1.2.1"]]
-  :dev-dependencies [[ring-mock "0.1.1"]])
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.1"]]}})
