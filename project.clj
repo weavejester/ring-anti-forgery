b7db0354c7f0eb80a6906be150907d231a1cd69f
James Reeves <jreeves@weavejester.com>
2012-07-27 18:33:18 +0100

    Added URL and license to project

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject ring-anti-forgery "0.2.0-SNAPSHOT"
   :description "Ring middleware to prevent CSRF attacks"
+  :url "https://github.com/weavejester/ring-anti-forgery"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [crypto-random "1.1.0"]]
   :profiles
