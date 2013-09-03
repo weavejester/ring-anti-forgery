7e105ad2952348b9a1d9355513166d3725f78b94
James Reeves <jreeves@weavejester.com>
2013-09-03 18:37:56 +0100

    Updated minimum supported Clojure version to 1.3.0

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/weavejester/ring-anti-forgery"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [crypto-random "1.1.0"]
                  [crypto-equality "0.1.0"]
                  [hiccup "1.0.0"]]
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.1"]]}
-   :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}})
+   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
