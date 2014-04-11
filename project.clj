57ded164a1f4323bf77e340e31c120eabbbdd381
James Reeves <jreeves@weavejester.com>
2014-04-11 17:56:42 +0100

    Updated dependencies to latest versions

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [crypto-random "1.1.0"]
-                 [crypto-equality "0.1.0"]
-                 [hiccup "1.0.0"]]
+                 [crypto-random "1.2.0"]
+                 [crypto-equality "1.0.0"]
+                 [hiccup "1.0.5"]]
   :profiles
-  {:dev {:dependencies [[ring-mock "0.1.1"]]}
+  {:dev {:dependencies [[ring-mock "0.1.5"]]}
    :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
    :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
