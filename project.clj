40e25311b94e97fb01a8bbe01b6d34ade8f87acb
James Reeves <jreeves@weavejester.com>
2014-05-19 22:52:09 +0100

    Released 1.0.0-RC1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring/ring-anti-forgery "0.3.2"
+(defproject ring/ring-anti-forgery "1.0.0-RC1"
   :description "Ring middleware to prevent CSRF attacks"
   :url "https://github.com/ring-clojure/ring-anti-forgery"
   :license {:name "The MIT License"
