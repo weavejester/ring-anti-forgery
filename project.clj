1f2e76a115ae86f3cd8ecdd2ef4b812255910f11
James Reeves <jreeves@weavejester.com>
2016-03-31 17:05:41 +0100

    Add test profiles for Clojure 1.7 and 1.8

--- project.clj
+++ project.clj
@@ -8,9 +8,11 @@
                  [crypto-equality "1.0.0"]
                  [hiccup "1.0.5"]]
   :plugins [[codox "0.8.5"]]
-  :aliases {"test-all" ["with-profile" "default:+1.4:+1.5:+1.6" "test"]}
+  :aliases {"test-all" ["with-profile" "default:+1.4:+1.5:+1.6:+1.7:+1.8" "test"]}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.5"]]}
    :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
    :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})
+   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+   :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+   :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}})
