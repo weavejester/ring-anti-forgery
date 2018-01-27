f80df28f281d158e69a2e936a27c7af1f3fff77f
James Reeves <jreeves@weavejester.com>
2018-01-27 19:35:53 +0000

    Remove ring.util.anti-forgery/anti-forgery-token
    It is clearer to force the *anti-forgery-token* var directly.

--- src/ring/util/anti_forgery.clj
+++ src/ring/util/anti_forgery.clj
@@ -1,15 +1,12 @@
 (ns ring.util.anti-forgery
   "Utility functions for inserting anti-forgery tokens into HTML forms."
-  (:require [hiccup.core :as h]
-            [hiccup.form :as hf]
-            [ring.middleware.anti-forgery :as anti-forgery]))
-
-(defn anti-forgery-token []
-  (force anti-forgery/*anti-forgery-token*))
+  (:require [hiccup.core :refer [html]]
+            [hiccup.form :refer [hidden-field]]
+            [ring.middleware.anti-forgery :refer [*anti-forgery-token*]]))
 
 (defn anti-forgery-field
   "Create a hidden field with the session anti-forgery token as its value.
   This ensures that the form it's inside won't be stopped by the anti-forgery
   middleware."
   []
-  (h/html (hf/hidden-field "__anti-forgery-token" (anti-forgery-token))))
+  (html (hidden-field "__anti-forgery-token" (force *anti-forgery-token*))))
