a8add00313ef273404b93f229a2ed797fa26b249
James Reeves <jreeves@weavejester.com>
2018-01-27 03:06:18 +0000

    Add Codox plugin

--- project.clj
+++ project.clj
@@ -7,6 +7,11 @@
                  [crypto-random "1.2.0"]
                  [crypto-equality "1.0.0"]
                  [hiccup "1.0.5"]]
+  :plugins [[lein-codox "0.10.3"]]
+  :codox
+  {:output-path "codox"
+   :project     {:name "Ring Anti-Forgery"}
+   :source-uri  "http://github.com/ring-clojure/ring-anti-forgery/blob/{version}/{filepath}#L{line}"}
   :aliases {"test-all" ["with-profile" "default:+1.6:+1.7:+1.8:+1.9" "test"]}
   :profiles
   {:dev {:dependencies [[ring/ring-mock "0.3.2"]]}
